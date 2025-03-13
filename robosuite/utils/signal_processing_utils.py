165481cbaac03e38f232f4c3d09680f76ce439a1
anselmo-parnada <anselmo.parnada@outlook.com>
2025-03-13 15:56:21 +0000

    replace bilinear low-pass filter with exponential low-pass filter

--- robosuite/utils/signal_processing_utils.py
+++ robosuite/utils/signal_processing_utils.py
@@ -28,34 +28,23 @@ class BackwardEulerDiff:
 
         return self.xd
 
-
 class LowPassFilter:
     """
-    Low pass filter implementation based on Tustin's bilinear discretisation
+    Low pass filter implementation based on Euler discretisation
     method.
     """
     def __init__(self, f_cutoff, dt):
         self.dt = dt
         self.f_cutoff = f_cutoff
-        omega_cutoff = 2.0 * np.pi * self.f_cutoff
         
-        # Compute transfer function
-        num = omega_cutoff
-        den = [1,omega_cutoff] 
-        lowPass = signal.TransferFunction(num,den)
+        self.tau = 1.0 / self.f_cutoff
 
-        # Discretise and obtain coefficients
-        discreteLowPass = lowPass.to_discrete(dt,method='gbt',alpha=0.5)
-        self.a1 = discreteLowPass.den[1]
-        self.b0 = discreteLowPass.num[0]
-        self.b1 = discreteLowPass.num[1]
+        self.alpha = self.dt / (self.tau + self.dt)
+        self.alpha_inv = 1.0 - self.alpha
 
         self.y = None
-        self.y_prev = None
-        self.x_prev = None
         self.temp_1 = None
         self.temp_2 = None
-        self.temp_3 = None
 
     def __call__(self, x):
         assert isinstance(x, np.ndarray), "Input must be an instance of np.ndarray"
@@ -63,22 +52,13 @@ class LowPassFilter:
         if self.y is None:
             self.y = x.copy()
 
-            self.y_prev = x.copy()
-            self.x_prev = x.copy()
-
             self.temp_1 = np.empty_like(x)
             self.temp_2 = np.empty_like(x)
-            self.temp_3 = np.empty_like(x)
             return self.y
         
-        np.multiply(self.a1, self.y_prev, out=self.temp_1)
-        np.multiply(self.b0, x, out=self.temp_2)
-        np.multiply(self.b1, self.x_prev, out=self.temp_3)
+        np.multiply(self.alpha_inv, self.y, out=self.temp_1)
+        np.multiply(self.alpha, x, out=self.temp_2)
 
         np.add(self.temp_1, self.temp_2, out=self.y)
-        np.add(self.y, self.temp_3, out=self.y)
-
-        self.y_prev[:] = self.y[:]
-        self.x_prev = x[:]
 
         return self.y
