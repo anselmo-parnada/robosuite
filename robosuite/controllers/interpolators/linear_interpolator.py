155ef012250aad773cde57e5340d678501a7c24b
anselmo-parnada <anselmo.parnada@outlook.com>
2025-07-20 17:01:03 +0100

    modified interpolator to match the same behaviour as real control stack

--- robosuite/controllers/interpolators/linear_interpolator.py
+++ robosuite/controllers/interpolators/linear_interpolator.py
@@ -120,17 +120,17 @@ class LinearInterpolator(Interpolator):
                 goal = T.mat2quat(T.euler2mat(self.goal))
 
             # Interpolate to the next sequence
-            x_current = T.quat_slerp(x, goal, fraction=(self.step + 1) / self.total_steps)
+            x_current = T.quat_slerp(x, goal, fraction=(self.step) / self.total_steps)
             if self.ori_interpolate == "euler":
                 # Map back to euler
                 x_current = T.mat2euler(T.quat2mat(x_current))
         else:
             # This is a normal interpolation
-            dx = (self.goal - x) / (self.total_steps - self.step)
-            x_current = x + dx
+            dx = (self.goal - x) / (self.total_steps)
+            x_current = x + dx * self.step
 
         # Increment step if there's still steps remaining based on ramp ratio
-        if self.step < self.total_steps - 1:
+        if self.step < self.total_steps:
             self.step += 1
 
         # Return the new interpolated step
